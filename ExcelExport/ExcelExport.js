cd9c53625f52ce261fc0af43f4893ffab48afa7c
Laszlo Molnar <molnar.laszlo@selester.hu>
2021-04-19 20:57:59 +0200

    Excel export done

--- ExcelExport/ExcelExport.js
+++ ExcelExport/ExcelExport.js
@@ -1,10 +1,19 @@
 import ExcelExportIcon from "./ExcelExport-icon.svg";
 import "./ExcelExport.scss";
+import XLSX from "xlsx";
+import FileSaver from 'file-saver';
 
 export default function ExcelExport(props) {
     function exportToExcel() {
-        alert("Export to Excel");
+        const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        const EXCEL_EXTENSION = '.xlsx';
+        const worksheet = XLSX.utils.json_to_sheet(props.data);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer = XLSX.write(workbook, {bookType: 'xlsx', type: 'array'});
+        const data = new Blob([excelBuffer], {type: EXCEL_TYPE});
+        FileSaver.saveAs(data, "excelData"+EXCEL_EXTENSION);
     }
+
     return (
         <div className="btn-excel-export" onClick={exportToExcel}>
             <img className="ExcelExport-icon" src={ExcelExportIcon} alt="ExcelExport-icon.svg" />
