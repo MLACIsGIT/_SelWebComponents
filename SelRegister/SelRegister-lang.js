2f8086d2199ad2f7d4dd1eff50407da5dbb1a378
Laszlo Molnar <molnar.laszlo@selester.hu>
2021-05-16 20:38:56 +0200

    Radio button object in InputFieldSet released

---
+++ SelRegister/SelRegister-lang.js
@@ -0,0 +1,70 @@
+export default function langJSON() {
+    return {
+        "header": {
+            "Description": "Language settings for Component SelRegister",
+            "last-modified": "2021-05-15"
+        },
+
+        "form-items": {
+            "field-name": {
+                "hu": "Az Ön neve:",
+                "de": "Ihre(n) Name:",
+                "en": "Your name:"
+            },
+
+            "field-company": {
+                "hu": "Cég neve:",
+                "de": "Firma:",
+                "en": "Company:"
+            },
+
+            "field-email": {
+                "hu": "Email:",
+                "de": "Email:",
+                "en": "Email:"
+            },
+
+            "field-gdprAccepted-before-link": {
+                "hu": "Az",
+                "de": "Ich habe",
+                "en": "I have read, understood and accept"
+            },
+
+            "field-gdprAccepted-link-text": {
+                "hu": "adatkezelési szabályzatot (GDPR)",
+                "de": "die Datenverwaltungsrichtlinien (GDPR)",
+                "en": "the data management policy (GDPR)"
+            },
+
+            "field-gdprAccepted-after-link": {
+                "hu": "elolvastam, megértettem és elfogadom.",
+                "de": "gelesen, verstanden und akzeptiert.",
+                "en": "."
+            },
+
+            "btn-register": {
+                "hu": "Regisztrálok",
+                "de": "Registieren",
+                "en": "Register"
+            },
+
+            "validation-required": {
+                "hu": "Kérem töltse ki a mezőt",
+                "de": "Dateneingabe erforderlick",
+                "en": "Please fill in the field"
+            },
+
+            "validation-chkboxChecked": {
+                "hu": "Kérem fogadja el az adatkezelési szabályzatot",
+                "de": "Bitte akzeptieren Sie die Datenverwaltungsrichtlinien",
+                "en": "Please accept the data management policy"
+            },
+
+            "no-response": {
+                "hu": "A szerviz nem reagál. Kérem próbálja később.",
+                "de": "Service antwortet nicht. Bitte versuchen Sie es später noch einmal.",
+                "en": "The service does not respond. Please try again later."
+            }
+        }
+    }
+}
